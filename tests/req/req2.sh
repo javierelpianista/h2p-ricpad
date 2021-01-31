7534994faaa81f1facb128862b084a3cb3656199
javierelpianista <javier.garcia.tw@hotmail.com>
2021-01-31 15:51:50 -0300

    Tidied up req examples

--- tests/req/req2.sh
+++ tests/req/req2.sh
@@ -6,10 +6,10 @@
     --A0 0.8 \
     --R0 2.0 \
     --d 2 \
-    --ndigits 1000 \
-    --h "1E-200" \
-    --hd "1E-400" \
-    --tol "1E-90" \
+    --ndigits 2000 \
+    --h "1E-400" \
+    --hd "1E-800" \
+    --tol "1E-120" \
     --nr-max-iter 10000 \
     --output-file d2.out \
     --log-nr 
